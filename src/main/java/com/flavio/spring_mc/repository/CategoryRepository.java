c93e777bbda6916117feb6ed69e0c82fc363cf3e
Git-Mon032 <evg11111992@gmail.com>
2025-03-12 10:37:59 +0900

    structure updated

--- src/main/java/com/flavio/spring_mc/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flavio.spring_mc.repository;
-
-import com.flavio.spring_mc.entities.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
